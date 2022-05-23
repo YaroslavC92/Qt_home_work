72528bb284fba4b0cd50c86db3f5ff4de0b9720f
YaroslavC92 <78161548+YaroslavC92@users.noreply.github.com>
2022-05-23 19:28:20 +0500

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,65 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QPrinter printer;
+    QPrintDialog dlg(&printer, this);
+    dlg.setWindowTitle("Print");
+    if (dlg.exec() != QDialog::Accepted)
+        return;
+    QString printStr = ui->plainTextEdit->toPlainText();
+    QChar *list = printStr.data();
+    QStringList strlst;
+    int line = 0, cursor = 0;
+    for (bool getst = true;getst;)
+    {
+        int index = printStr.indexOf("\n", cursor);
+        QString s = "";
+        if (index == -1)
+        {
+            getst = false;
+            s.append(&list[cursor], printStr.length() - cursor);
+        }
+        else s.append(&list[cursor], index - cursor);
+        cursor = index + 1;
+        strlst << s;
+    }
+    QPainter painter;
+    painter.begin(&printer);
+    int w = painter.window().width();
+    int h = painter.window().height();
+    int amount = strlst.count();
+    QFont font = painter.font();
+    QFontMetrics fmetrics(font);
+    for (int i = 0; i < amount; i++)
+    {
+        QPointF pf;
+        pf.setX(10);
+        pf.setY(line);
+        painter.drawText(pf, strlst.at(i));
+        line += fmetrics.height();
+        if (h - line <= fmetrics.height())
+        {
+            printer.newPage();
+            line = 0;
+        }
+    }
+    painter.end();
+
+
+}
+
